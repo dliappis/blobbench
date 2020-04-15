558aaeccd06d31731afa38d7d93273b129a9f7eb
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2020-04-15 10:58:37 +0300

    WiP 2

--- cmd/download.go
+++ cmd/download.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"sync"
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/service/s3"
@@ -17,23 +20,65 @@ type serviceTime struct {
 	LastGet  time.Duration
 }
 
-var bufferSize uint64 = 1024 * 8 // 8 kilobytes
+const bufferSize uint64 = 1024 * 8 // 8 kilobytes
 
 var (
 	basedir                string
 	prefix                 string
 	suffixseparator        string
-	numfiles, suffixdigits int
-	numtasks               int
+	numFiles, suffixdigits int
+	numWorkers             int
 	bufsize                uint64
+
+	wg    sync.WaitGroup
+	queue = make(chan taskStruct, numWorkers)
 
 	downloadCmd = &cobra.Command{
 		Use:   "download",
 		Short: "Download ...",
 		Long:  `TODO`,
-		Run:   download,
+		Run:   initDownload,
 	}
 )
+
+type worker struct {
+	id int
+	ch <-chan taskStruct
+	wg *sync.WaitGroup
+}
+
+type taskStruct struct {
+	workerFunc func(int, *s3.Client, chan<- serviceTime)
+	filesuffix int
+	results    chan<- serviceTime
+	s3client   *s3.Client
+}
+
+func (w *worker) run() {
+	go func() {
+		defer w.wg.Done()
+		for tStruct := range w.ch {
+			// call the work function here
+			color.Yellow("About to start %d", tStruct.filesuffix)
+			tStruct.workerFunc(tStruct.filesuffix, tStruct.s3client, tStruct.results)
+		}
+	}()
+}
+
+func add(ctx context.Context, task taskStruct) error {
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case queue <- task:
+	}
+	return nil
+}
+
+func wait() error {
+	close(queue)
+	wg.Wait()
+	return nil
+}
 
 func init() {
 	rootCmd.AddCommand(downloadCmd)
@@ -42,100 +87,97 @@ func init() {
 	downloadCmd.MarkFlagRequired("basedir")
 	downloadCmd.Flags().StringVar(&prefix, "prefix", "", "The prefix of the filename")
 	downloadCmd.MarkFlagRequired("prefix")
-	downloadCmd.Flags().IntVar(&numfiles, "numfiles", 2, "How many files")
+	downloadCmd.Flags().IntVar(&numFiles, "numfiles", 2, "How many files")
 	downloadCmd.MarkFlagRequired("numfiles")
 	downloadCmd.Flags().IntVar(&suffixdigits, "suffixdigits", 4, "suffix digits, should start from 0, e.g. 4 for -0000")
 	downloadCmd.MarkFlagRequired("suffixdigits")
 	downloadCmd.Flags().StringVar(&suffixseparator, "suffixsep", "-", "The separator for suffix e.g. 0 for -0000.")
 
-	downloadCmd.Flags().IntVar(&numtasks, "tasks", 5, "Amount of parallel download workers")
+	downloadCmd.Flags().IntVar(&numWorkers, "workers", 5, "Amount of parallel download workers")
 	downloadCmd.Flags().Uint64Var(&bufsize, "bufsize", 1024, "Buf size to use while download (per worker)")
 
 }
 
-func download(cmd *cobra.Command, args []string) {
-	color.Green(">>> Starting download benchmarks")
-	var allresults []serviceTime
-	results := make(chan serviceTime, numfiles)
-	s3client := s3.New(internal.SetupS3Client(Region))
-	taskGroupChunks := numfiles / numtasks
-
-	for taskGroupIdx := 1; taskGroupIdx <= taskGroupChunks+1; taskGroupIdx++ {
-		tasks := numtasks
-		if taskGroupIdx == taskGroupChunks+1 {
-			// last batch may include less tasks that numtasks
-			tasks = numfiles % numtasks
-		}
-
-		tasksPool := make(chan int, tasks)
-		for curChan := 1; curChan <= tasks; curChan++ {
-			go downloadTask(taskGroupIdx, s3client, tasksPool, results)
-		}
-
-		for j := 1; j <= tasks; j++ {
-			color.Red("Spawning task %d", j)
-			tasksPool <- ((taskGroupIdx - 1) * numtasks) + (j - 1)
-		}
-		close(tasksPool)
-		// collect results
-		for s := 0; s < tasks; s++ {
-			allresults = append(allresults, <-results)
-		}
-
+func initDownload(cmd *cobra.Command, args []string) {
+	color.Green("Initializing pool with workers")
+	for i := 1; i <= numWorkers; i++ {
+		w := worker{id: i, ch: queue, wg: &wg}
+		w.run()
 	}
-	// print results
-	for _, ss := range allresults {
+	wg.Add(numWorkers)
+
+	// maybe move this up
+	s3client := s3.New(internal.SetupS3Client(Region))
+
+	results := make(chan serviceTime, numWorkers*numFiles)
+
+	for i := 1; i <= numFiles; i++ {
+		ctx := context.Background()
+
+		tStruct := taskStruct{filesuffix: i, results: results, s3client: s3client, workerFunc: downloadTask}
+		if err := add(ctx, tStruct); err != nil {
+			color.Red("ERROR: Adding item: %s", err)
+			os.Exit(1)
+		}
+	}
+
+	if err := wait(); err != nil {
+		color.Red("ERROR: Close: %s", err)
+	}
+
+	color.Green(">>> Threadpool exited")
+	close(results)
+
+	for ss := range results {
 		color.Green("Sample %d, Suffix: %s, FirstGet %v, LastGet %v", ss.index, ss.file, ss.FirstGet, ss.LastGet)
 	}
 
 }
 
-func downloadTask(taskGroupIdx int, s3client *s3.Client, tasks <-chan int, results chan<- serviceTime) {
-	for i := range tasks {
-		key := fmt.Sprintf("%s/%s%s%0*d", basedir, prefix, suffixseparator, suffixdigits, i)
-		color.HiMagenta("DEBUG working on group [%d] file [%s/%s]", taskGroupIdx, BucketName, key)
-		stopwatch := time.Now()
-		time.Sleep(1 * time.Second)
-		// req := s3client.GetObjectRequest(&s3.GetObjectInput{
-		// 	Bucket: aws.String(BucketName),
-		// 	Key:    aws.String(key),
-		// })
+func downloadTask(filesuffix int, s3client *s3.Client, results chan<- serviceTime) {
+	key := fmt.Sprintf("%s/%s%s%0*d", basedir, prefix, suffixseparator, suffixdigits, filesuffix)
+	color.HiMagenta("DEBUG working on file [%s/%s]", BucketName, key)
+	stopwatch := time.Now()
+	time.Sleep(1 * time.Second)
+	// req := s3client.GetObjectRequest(&s3.GetObjectInput{
+	// 	Bucket: aws.String(BucketName),
+	// 	Key:    aws.String(key),
+	// })
 
-		// resp, err := req.Send(context.Background())
+	// resp, err := req.Send(context.Background())
 
-		// if err != nil {
-		// 	panic("Failed to get object: " + err.Error())
-		// }
+	// if err != nil {
+	// 	panic("Failed to get object: " + err.Error())
+	// }
 
-		firstGet := time.Now().Sub(stopwatch)
+	firstGet := time.Now().Sub(stopwatch)
 
-		// // create a buffer to copy the S3 object body to
-		// // TODO specify somewhere the buffer size, for now it's 1MB
-		// var buf = make([]byte, bufferSize)
+	// // create a buffer to copy the S3 object body to
+	// // TODO specify somewhere the buffer size, for now it's 1MB
+	// var buf = make([]byte, bufferSize)
 
-		// // read the s3 object body into the buffer
-		// size := 0
-		// for {
-		// 	n, err := resp.Body.Read(buf)
+	// // read the s3 object body into the buffer
+	// size := 0
+	// for {
+	// 	n, err := resp.Body.Read(buf)
 
-		// 	size += n
+	// 	size += n
 
-		// 	if err == io.EOF {
-		// 		break
-		// 	}
+	// 	if err == io.EOF {
+	// 		break
+	// 	}
 
-		// 	// if the streaming fails, exit
-		// 	if err != nil {
-		// 		// TODO see https://docs.aws.amazon.com/sdk-for-go/v1/developer-guide/handling-errors.html
-		// 		panic("Error reading object body: " + err.Error())
-		// 	}
-		// }
+	// 	// if the streaming fails, exit
+	// 	if err != nil {
+	// 		// TODO see https://docs.aws.amazon.com/sdk-for-go/v1/developer-guide/handling-errors.html
+	// 		panic("Error reading object body: " + err.Error())
+	// 	}
+	// }
 
-		// _ = resp.Body.Close()
+	// _ = resp.Body.Close()
 
-		lastGet := time.Now().Sub(stopwatch)
+	lastGet := time.Now().Sub(stopwatch)
 
-		// send measurements
-		results <- serviceTime{FirstGet: firstGet, LastGet: lastGet, index: i, file: fmt.Sprintf("%s", key)}
-	}
+	// send measurements
+	results <- serviceTime{FirstGet: firstGet, LastGet: lastGet, index: filesuffix, file: fmt.Sprintf("%s", key)}
 }
