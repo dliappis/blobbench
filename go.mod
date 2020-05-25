d2d91725386c72275e5977adfa54494431b65d34
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2020-05-25 16:03:37 +0300

    Experiment setting ReadBufferSize in GCS

--- go.mod
+++ go.mod
@@ -9,5 +9,6 @@ require (
 	github.com/mitchellh/gox v1.0.1 // indirect
 	github.com/spf13/cobra v1.0.0
 	golang.org/x/net v0.0.0-20200222125558-5a598a2470a0
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
 	google.golang.org/api v0.18.0
 )
--- internal/gcshelper.go
+++ internal/gcshelper.go
@@ -2,6 +2,9 @@ package internal
 
 import (
 	"context"
+	"fmt"
+	"reflect"
+	"unsafe"
 
 	"cloud.google.com/go/storage"
 )
@@ -10,9 +13,35 @@ import (
 func SetupGCSClient() *storage.Client {
 	ctx := context.Background()
 
+	// hc, _ := google.DefaultClient(ctx, storage.ScopeReadOnly)
+
 	client, err := storage.NewClient(ctx)
+
 	if err != nil {
 		panic("Failed to create client: " + err.Error())
 	}
+
+	c := reflect.ValueOf(client).Elem()
+	hc := c.FieldByName("hc").Elem()
+	fmt.Printf("HC: %#v\n", hc)
+	tp := hc.FieldByName("Transport").Elem()
+
+	base := reflect.Indirect(tp).FieldByName("Base").Elem()
+	octp := reflect.Indirect(base).FieldByName("Base").Elem()
+	realtp := reflect.Indirect(octp).FieldByName("base").Elem()
+
+	fmt.Printf("HC: %#v\n", tp)
+	fmt.Printf("HC: %#v\n", base)
+	fmt.Printf("HC: %#v\n", octp)
+	fmt.Printf("HC: %#v\n", realtp)
+
+	bufSize := reflect.Indirect(realtp).FieldByName("ReadBufferSize")
+
+	fmt.Printf("HC: %#v\n", bufSize)
+
+	rf := reflect.NewAt(bufSize.Type(), unsafe.Pointer(bufSize.UnsafeAddr())).Elem()
+	rf.SetInt(2 * 1024 * 1024)
+	fmt.Printf("HC: %#v\n", bufSize)
+
 	return client
 }
